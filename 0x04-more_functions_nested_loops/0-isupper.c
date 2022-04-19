544332a98f43056a56216585eef338907457d6d5
Jaybee93 <j.khonzeka@gmail.com>
2022-04-19 00:21:09 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isupper - checkes upper case
- * @c: print int
- * Return: give 1 or 0
+ * _isupper - evalue if letter is uppercase.
+ *@c: print int
+ * Return: Always 0.
  */
 
 int _isupper(int c)
@@ -13,8 +13,7 @@ return (1);
 }
 
 else
-{
-return (0);
+{return (0);
 }
 
 }
